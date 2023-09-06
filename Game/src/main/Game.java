ab5592eb1ca0c13074b242567c26ed9813e76deb
VusalGuliyevv <vsalquliyev@gmail.com>
2023-09-06 18:47:48 +0400

    Game class has been created.The start method has been added inside it to begin the game.

---
+++ Game/src/main/Game.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Scanner;
+
+public class Game {
+		static void start() {
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.println("\nHello! Welcome to the Bomb Game.\n" +
+	                "In this game, you select a range and guess numbers within the range.\n" +
+	                "One of the numbers contains a bomb.\n" +
+	                "If you select it, it's a BOMB.\n" +
+	                "Do you want to play?\n" +
+	                "1) Yes  2) No");
+			int startGame = scanner.nextInt();
+		}
+		
+}
